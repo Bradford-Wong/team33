c39e447fc80b2fce0692c0e921d5fb65429b7ee9
Ben <catsrnice@hotmail.com>
2017-11-10 20:58:52 -0800

    Debugged
    Fixed a bug

--- src/main/java/models/Column.java
+++ src/main/java/models/Column.java
@@ -13,30 +13,12 @@ public class Column {
 
     public boolean removeCard() {
         if(columnHasCards()) {
-            Card c = getTopCard();
-            boolean removeCard = false;
-            // Error e = new Error();
-
-            if (columnHasCards()) {
-                Card compare = getTopCard();
-                //System.out.println(compare);
-                if (compare.getSuit() == c.getSuit()) {
-                    if (compare.getValue() > c.getValue()) {
-                        removeCard = true;
-                    }
-                }
-            }
-
-            if (removeCard) {
                 this.col.remove(this.col.size() - 1);
                 return true;
-            }
-            else {
-                Error e = new Error();
-                e.infoBox("A card can only be removed when another card is showing at the top of another pile with the same suit and higher value!", "Remove Error");
-                return false;
-            }
-        } else {
+        }
+        else {
+            Error e = new Error();
+            e.infoBox("A card can only be removed when another card is showing at the top of another pile with the same suit and higher value!", "Remove Error");
             return false;
         }
     }
--- src/main/java/models/Game.java
+++ src/main/java/models/Game.java
@@ -1,11 +1,14 @@
+
+
+
 package models;
 import  models.Error;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Random;
-        import javax.swing.JButton;
-        import javax.swing.JFrame;
-        import javax.swing.JOptionPane;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 /**
  * Assignment 1: Students must implement dealFour(), remove(), move(), and columnHasCards() methods
  *
@@ -17,7 +20,7 @@ public class Game {
 
     public java.util.List<Card> deck = new ArrayList<>();
 
-    public java.util.List<Column> cols = new ArrayList<>();
+    private java.util.List<Column> cols = new ArrayList<>();
 
 
     public Game(){
@@ -34,6 +37,22 @@ public class Game {
             deck.add(new Card(i,Suit.Diamonds));
             deck.add(new Card(i,Suit.Spades));
         }
+    }
+
+    public java.util.List<Card> getcol1() {    //This is because apparantly the routing backend has trouble converting the columns to json
+        return cols.get(0).col;
+    }
+
+    public java.util.List<Card> getcol2() {    //This is because apparantly the routing backend has trouble converting the columns to json
+        return cols.get(1).col;
+    }
+
+    public java.util.List<Card> getcol3() {    //This is because apparantly the routing backend has trouble converting the columns to json
+        return cols.get(2).col;
+    }
+
+    public java.util.List<Card> getcol4() {    //This is because apparantly the routing backend has trouble converting the columns to json
+        return cols.get(3).col;
     }
 
     public void shuffle() {
@@ -61,21 +80,50 @@ public class Game {
     }
 
     public void remove(int columnNumber) {
-        for (int i = 0; i < 4; i++) {
-            if (i != columnNumber) {
-                cols.get(i).removeCard();
+        if(cols.get(columnNumber).columnHasCards()) {
+            Card c = cols.get(columnNumber).getTopCard();
+            boolean removeCard = false;
+            // Error e = new Error();
+            for (int i = 0; i < 4; i++) {
+                if (i != columnNumber) {
+                    if (cols.get(i).columnHasCards()) {
+                        Card compare = cols.get(i).getTopCard();
+                        //System.out.println(compare);
+                        if (compare.getSuit() == c.getSuit()) {
+                            if (compare.getValue() > c.getValue()) {
+                                removeCard = true;
+                            }
+                        }
+                    }
+                }
+            }
+            if (removeCard) {
+                this.cols.get(columnNumber).removeCard();
+            }
+            else {
+                Error e = new Error();
+                e.infoBox("A card can only be removed when another card is showing at the top of another pile with the same suit and higher value!", "Remove Error");
             }
         }
     }
 
 
     public void move(int columnFrom, int columnTo) {
-        //
+        System.out.println("GASP1");
         if(cols.get(columnFrom).columnHasCards()){
+            System.out.println("GASP2");
             Card cardToMove = cols.get(columnFrom).getTopCard();
+            System.out.println(!cols.get(columnTo).columnHasCards());
+            System.out.println(cardToMove.getValue() == 14);
+            System.out.println(cols.get(columnFrom).columnHasCards());
+
             if(!cols.get(columnTo).columnHasCards() && cardToMove.getValue() == 14 && cols.get(columnFrom).columnHasCards()){ //check that moving to empty column. Will need to change this line a bit when refactor
-                this.removeCardFromCol(columnFrom);
-                this.addCardToCol(columnTo,cardToMove);
+                //System.out.println(compare);
+                System.out.println("GASP2.5");
+                cols.get(columnFrom).removeCard();
+                System.out.println("GASP3");
+                cols.get(columnTo).addCardToCol(cardToMove);
+                System.out.println("SUCCESS");
             }else { //When make an invalid move when there is a card in the column that moving from
                 Error e = new Error();
                 if(columnFrom == columnTo){
@@ -101,16 +149,5 @@ public class Game {
 
 
     }
-
-    private void addCardToCol(int columnTo, Card cardToMove) {
-        cols.get(columnTo).addCardToCol(cardToMove);
-    }
-
-    private void removeCardFromCol(int colFrom) {
-        this.cols.get(colFrom).removeCard();
-    }
-
-
-
 }
 
