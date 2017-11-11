d4ac87ce35df8a6114be1d53e0071a6b794a42d4
Ben <catsrnice@hotmail.com>
2017-11-10 17:02:17 -0800

    Started column class

---
+++ src/main/java/models/Column.java
@@ -0,0 +1,60 @@
+package models;
+
+import java.util.ArrayList;
+
+public class Column {
+
+    public java.util.List<Card> col = new ArrayList<Card>();
+
+    public Column(){
+
+    }
+
+
+    public boolean remove() {
+        if(columnHasCards()) {
+            Card c = getTopCard();
+            boolean removeCard = false;
+            // Error e = new Error();
+
+            if (columnHasCards()) {
+                Card compare = getTopCard();
+                //System.out.println(compare);
+                if (compare.getSuit() == c.getSuit()) {
+                    if (compare.getValue() > c.getValue()) {
+                        removeCard = true;
+                    }
+                }
+            }
+
+            if (removeCard) {
+                this.col.remove(this.col.size() - 1);
+                return true;
+            }
+            else {
+                Error e = new Error();
+                e.infoBox("A card can only be removed when another card is showing at the top of another pile with the same suit and higher value!", "Remove Error");
+                return false;
+            }
+        } else {
+            return false;
+        }
+    }
+
+    public boolean columnHasCards() {
+        if(this.col.size()>0){
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public Card getTopCard() {
+        return this.col.get(this.col.size()-1);
+    }
+
+    public void addCardToCol(Card cardToMove) {
+        col.add(cardToMove);
+    }
+
+}
--- src/main/java/models/Game.java
+++ src/main/java/models/Game.java
@@ -17,15 +17,7 @@ public class Game {
 
     public java.util.List<Card> deck = new ArrayList<>();
 
-    public java.util.List<java.util.List<Card>> cols = new ArrayList<>();
-
-
-    public Game(){
-        cols.add(new ArrayList<Card>());
-        cols.add(new ArrayList<Card>());
-        cols.add(new ArrayList<Card>());
-        cols.add(new ArrayList<Card>());
-    }
+    public java.util.List<Column> cols = new ArrayList<>();
 
     public void buildDeck() {
         for(int i = 2; i < 15; i++){
@@ -42,71 +34,38 @@ public class Game {
     }
 
     public void dealFour() {
-
         for(int i = 0; i < 4; i++){
-            cols.get(i).add(deck.get(deck.size()-1));
+            cols.get(i).addCardToCol(deck.get(deck.size()-1));
             deck.remove(deck.size()-1);
         }
     }
 
     //customDeal to setup game for testing purposes
     public void customDeal(int c1, int c2, int c3, int c4) {
-        cols.get(0).add(deck.get(c1));
+        cols.get(0).addCardToCol(deck.get(c1));
         deck.remove(c1);
-        cols.get(1).add(deck.get(c2));
+        cols.get(1).addCardToCol(deck.get(c2));
         deck.remove(c2);
-        cols.get(2).add(deck.get(c3));
+        cols.get(2).addCardToCol(deck.get(c3));
         deck.remove(c3);
-        cols.get(3).add(deck.get(c4));
+        cols.get(3).addCardToCol(deck.get(c4));
         deck.remove(c4);
     }
 
     public void remove(int columnNumber) {
-        if(columnHasCards(columnNumber)) {
-            Card c = getTopCard(columnNumber);
-            boolean removeCard = false;
-           // Error e = new Error();
-            for (int i = 0; i < 4; i++) {
-                if (i != columnNumber) {
-                    if (columnHasCards(i)) {
-                        Card compare = getTopCard(i);
-                        //System.out.println(compare);
-                        if (compare.getSuit() == c.getSuit()) {
-                            if (compare.getValue() > c.getValue()) {
-                                removeCard = true;
-                            }
-                        }
-                    }
-                }
-            }
-            if (removeCard) {
-                this.cols.get(columnNumber).remove(this.cols.get(columnNumber).size() - 1);
-            }
-            else {
-                Error e = new Error();
-                e.infoBox("A card can only be removed when another card is showing at the top of another pile with the same suit and higher value!", "Remove Error");
+        for (int i = 0; i < 4; i++) {
+            if (i != columnNumber) {
+                cols.get(i).remove();
             }
         }
-    }
-
-    private boolean columnHasCards(int columnNumber) {
-        if(this.cols.get(columnNumber).size()>0){
-            return true;
-        }
-        return false;
-
-    }
-
-    private Card getTopCard(int columnNumber) {
-        return this.cols.get(columnNumber).get(this.cols.get(columnNumber).size()-1);
     }
 
 
     public void move(int columnFrom, int columnTo) {
         //
-        if(columnHasCards(columnFrom)){
-            Card cardToMove = getTopCard(columnFrom);
-            if(!columnHasCards(columnTo) && cardToMove.getValue() == 14 && columnHasCards(columnFrom)){ //check that moving to empty column. Will need to change this line a bit when refactor
+        if(cols.get(columnFrom).columnHasCards()){
+            Card cardToMove = cols.get(columnFrom).getTopCard();
+            if(!cols.get(columnTo).columnHasCards() && cardToMove.getValue() == 14 && cols.get(columnFrom).columnHasCards()){ //check that moving to empty column. Will need to change this line a bit when refactor
                 this.removeCardFromCol(columnFrom);
                 this.addCardToCol(columnTo,cardToMove);
             }else { //When make an invalid move when there is a card in the column that moving from
@@ -114,16 +73,16 @@ public class Game {
                 if(columnFrom == columnTo){
                     e.infoBox("Can't move to the same column!", "Moving Error");
                 }
-                if(columnHasCards(columnTo)){
+                if(cols.get(columnTo).columnHasCards()){
                     e.infoBox("This spot isn't empty to move the card to!", "Moving Error");
 
                 }
-                if(!columnHasCards(columnTo) && cardToMove.getValue() != 14 && columnHasCards(columnFrom)){
+                if(!cols.get(columnTo).columnHasCards() && cardToMove.getValue() != 14 && cols.get(columnFrom).columnHasCards()){
                     e.infoBox("Can't move non-Ace Cards!", "Moving Error");
                 }
             }
         }else{ //when the column moving from is empty
-            if(columnHasCards(columnFrom) == false){
+            if(cols.get(columnFrom).columnHasCards() == false){
                 Error e = new Error();
                 e.infoBox("This spot doesn't have any cards to move from!","Moving Error");
 
@@ -136,11 +95,11 @@ public class Game {
     }
 
     private void addCardToCol(int columnTo, Card cardToMove) {
-        cols.get(columnTo).add(cardToMove);
+        cols.get(columnTo).addCardToCol(cardToMove);
     }
 
     private void removeCardFromCol(int colFrom) {
-        this.cols.get(colFrom).remove(this.cols.get(colFrom).size()-1);
+        this.cols.get(colFrom).remove();
     }
 
 
